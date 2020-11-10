55a69fe484f2aae441937aa345f5640f5546dbae
Charlie Kelson <charlie.kelson@gettyimages.com>
2020-11-09 18:37:27 -0800

    Add requirements for assignment 7

--- blogging/admin.py
+++ blogging/admin.py
@@ -1,5 +1,21 @@
 from django.contrib import admin
 from blogging.models import Post, Category
 
-admin.site.register(Post)
-admin.site.register(Category)
+
+class CategoryInline(admin.StackedInline):
+    model = Category.posts.through
+
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    inlines = [
+        CategoryInline,
+    ]
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = [
+        'posts',
+    ]
+
