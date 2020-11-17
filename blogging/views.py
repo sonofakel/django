c01b84d3fca496b913302524ec886f56d5434a91
Charlie Kelson <charlie.kelson@gettyimages.com>
2020-11-16 20:02:59 -0800

    Add queryset

--- blogging/views.py
+++ blogging/views.py
@@ -1,43 +1,13 @@
-from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django.shortcuts import render
 from blogging.models import Post
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 
 
 class PostListView(ListView):
-    model = Post
+    queryset = Post.objects.order_by('-created_date')
     template_name = 'blogging/list.html'
 
 
 class PostDetailView(DetailView):
     model = Post
     template_name = 'blogging/detail.html'
-
-
-# def stub_view(request, *args, **kwargs):
-#     body = "Stub View\n\n"
-#     if args:
-#         body += "Args:\n"
-#         body += "\n".join(["\t%s" % a for a in args])
-#     if kwargs:
-#         body += "Kwargs:\n"
-#         body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
-#     return HttpResponse(body, content_type="text/plain")
-#
-#
-# def list_view(request):
-#     published = Post.objects.exclude(published_date__exact=None)
-#     posts = published.order_by('-published_date')
-#     context = {'posts': posts}
-#     return render(request, 'blogging/list.html', context)
-#
-#
-# def detail_view(request, post_id):
-#     published = Post.objects.exclude(published_date__exact=None)
-#     try:
-#         post = published.get(pk=post_id)
-#     except Post.DoesNotExist:
-#         raise Http404
-#     context = {'post': post}
-#     return render(request, 'blogging/detail.html', context)
