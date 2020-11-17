5beb9c5a171fe225b23b6a1f72d8fb6d599fefd4
Charlie Kelson <charlie.kelson@gettyimages.com>
2020-11-16 19:52:15 -0800

    Add class based views

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
-from blogging.views import list_view, detail_view
-
+from blogging.views import PostDetailView, PostListView
 urlpatterns = [
-    path('', list_view, name="blog_index"),
-    path('posts/<int:post_id>/', detail_view, name="blog_detail"),
+    path('', PostListView.as_view(), name="blog_index"),
+    path('posts/<int:pk>/', PostDetailView.as_view(), name="blog_detail"),
     ]
